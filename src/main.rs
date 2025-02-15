24de0717efeac5844815cbdeef2d310440283527
Tom Planche <Tomplanche@icloud.com>
2025-02-15 22:52:20 +0100

    [2] (feat on master) WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-fn main() {
+use async_lastfm::lastfm_handler::{LastFMHandler, TrackLimit};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok();
 
-    println!("Hello, world!");
+    let handler = LastFMHandler::new("tom_planche");
+
+    let path = handler
+        .export_recent_play_counts(TrackLimit::Limited(100))
+        .await?;
+
+    println!("Exported play counts to: {}", path);
+
+    Ok(())
 }
