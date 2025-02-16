d788c17cb2c445350eeefbee834fc7d1dffcf6a6
Tom Planche <Tomplanche@icloud.com>
2025-02-16 03:44:03 +0100

    [3] (feat on main) Added cron for auto-run.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
 use async_lastfm::lastfm_handler::{LastFMHandler, TrackLimit};
+use chrono::Utc;
+use cron::Schedule;
+use std::str::FromStr;
+use tokio::time::{sleep, Duration};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
     dotenv::dotenv().ok();
 
     let handler = LastFMHandler::new("tom_planche");
 
-    let path = handler
-        .export_recent_play_counts(TrackLimit::Limited(100))
-        .await?;
+    let expression = "0 0/5 * * * *"; // Every 5 minutes
+    let schedule = Schedule::from_str(expression).expect("Failed to parse CRON expression");
 
-    println!("Exported play counts to: {}", path);
+    loop {
+        let now = Utc::now();
+        if let Some(next) = schedule.upcoming(Utc).take(1).next() {
+            let until_next = next - now;
+            sleep(Duration::from_secs(
+                u64::try_from(until_next.num_seconds()).unwrap_or_default(),
+            ))
+            .await;
 
-    Ok(())
+            if let Err(e) = handler
+                .export_recent_play_counts(TrackLimit::Limited(100))
+                .await
+            {
+                eprintln!("Failed to export recent play counts: {e:?}");
+            }
+        }
+    }
 }
