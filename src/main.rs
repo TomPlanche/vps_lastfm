7f93a8051814bf20663dc1a82aad968c9a535c88
Tom Planche <Tomplanche@icloud.com>
2025-02-27 22:34:11 +0100

    [6] (feat on main) Added a new `fetch_current_track` + updated dependencies.

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@ use cron::Schedule;
 use std::str::FromStr;
 use tokio::time::{sleep, Duration};
 
-#[tokio::main]
-async fn main() {
-    dotenv::dotenv().ok();
-
-    let handler = LastFMHandler::new("tom_planche");
-
-    let expression = "0 0/5 * * * *"; // Every 5 minutes
+///
+/// # `fetch_recent_play_counts`
+/// Fetches the recent play counts from Last.fm and exports them to a JSON file.
+///
+/// ## Arguments
+/// * `handler` - A reference to the LastFMHandler instance.
+async fn fetch_recent_play_counts(handler: &LastFMHandler) {
+    let expression = "0 0/1 * * * *"; // Every minutes
     let schedule = Schedule::from_str(expression).expect("Failed to parse CRON expression");
 
     loop {
@@ -30,4 +31,45 @@ async fn main() {
             }
         }
     }
+}
+
+///
+/// # `fetch_current_track`
+/// Fetches the current track from Last.fm and exports it to a JSON file.
+///
+/// ## Arguments
+/// * `handler` - A reference to the LastFMHandler instance.
+async fn fetch_current_track(handler: &LastFMHandler) {
+    let expression = "0/5 * * * * *"; // Each 5 seconds
+    let schedule = Schedule::from_str(expression).expect("Failed to parse CRON expression");
+
+    loop {
+        let now = Utc::now();
+        if let Some(next) = schedule.upcoming(Utc).take(1).next() {
+            let until_next = next - now;
+            sleep(Duration::from_secs(
+                u64::try_from(until_next.num_seconds()).unwrap_or_default(),
+            ))
+            .await;
+
+            if let Err(e) = handler
+                .update_recent_play_counts(TrackLimit::Limited(1), "data/currently_listening.json")
+                .await
+            {
+                eprintln!("Failed to export current track: {e:?}");
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let handler = LastFMHandler::new("tom_planche");
+
+    tokio::join!(
+        fetch_recent_play_counts(&handler),
+        fetch_current_track(&handler)
+    );
 }
