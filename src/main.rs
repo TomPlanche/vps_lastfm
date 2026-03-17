24287d4cdf618e21cdd30b84a1a34a42674b95c1
Tom Planche <tomplanche@proton.me>
2026-03-17 13:14:36 +0100

    [18] (chore on main) Add `update_scrobbles_db` expression loop

--- src/main.rs
+++ src/main.rs
@@ -134,12 +134,26 @@ async fn update_top_tracks_gist(client: &LastFmClient, username: &str, cfg: &Con
 }
 
 async fn update_scrobbles_db(client: &LastFmClient, username: &str, db_file: &str) {
-    if let Err(e) = client
-        .recent_tracks(username)
-        .fetch_and_update_sqlite(db_file)
-        .await
-    {
-        eprintln!("Failed to update scrobbles db: {e:?}");
+    let expression = "0 0/1 * * * *"; // Every minute
+    let schedule = Schedule::from_str(expression).expect("Failed to parse CRON expression");
+
+    loop {
+        let now = Utc::now();
+        if let Some(next) = schedule.upcoming(Utc).take(1).next() {
+            let until_next = next - now;
+            sleep(Duration::from_secs(
+                u64::try_from(until_next.num_seconds()).unwrap_or_default(),
+            ))
+            .await;
+
+            if let Err(e) = client
+                .recent_tracks(username)
+                .fetch_and_update_sqlite(db_file)
+                .await
+            {
+                eprintln!("Failed to update scrobbles db: {e:?}");
+            }
+        }
     }
 }
 
